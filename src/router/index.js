da8ae320eb60d88c5b9c1915590a9b1e9f992b3e
Christopher Wray <53663762+cwray-tech@users.noreply.github.com>
2020-10-29 21:41:17 -0700

    Updated routes + login page and bootstrap

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,28 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
+    path: '/',
+    name: 'Home',
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/login',
+    name: 'Login',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  routes
-});
+  routes,
+  mode: 'history'
+})
 
-export default router;
+export default router
